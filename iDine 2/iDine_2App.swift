5ce15aa612929fe206801155f37f9413062a4638
JonathanVarghese <jvarghese@bsd220.org>
2024-09-09 09:24:03 -0500

    Initial Commit

---
+++ iDine 2/iDine_2App.swift
@@ -0,0 +1,17 @@
+//
+//  iDine_2App.swift
+//  iDine 2
+//
+//  Created by Jonathan V on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iDine_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
